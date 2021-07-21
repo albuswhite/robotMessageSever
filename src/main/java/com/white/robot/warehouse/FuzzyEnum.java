81a82b18431df975f2943844e34118de1b41fd7b
whitechen <1250473565@qq.com>
2021-07-21 20:30:14 +0800

    --增加语料

--- src/main/java/com/white/robot/warehouse/FuzzyEnum.java
+++ src/main/java/com/white/robot/warehouse/FuzzyEnum.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum FuzzyEnum {
     M1("男女比", "果园男女比2:1，当然脱单和男女比无关，具体请参见新生手册"),
     M19("学妹", "学妹小心，不要听怪叔叔的话"),
+    M21("学姐","请加可爱的白白学姐"),
     M20("学弟", "学弟这个物种，懂得都懂");
 
     private final String req;
