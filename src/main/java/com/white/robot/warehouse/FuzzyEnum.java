c323ede1d1ad1cd217d3fae641e42b9c8f428de8
whitechen <1250473565@qq.com>
2021-07-22 14:12:33 +0800

    --增加语料

--- src/main/java/com/white/robot/warehouse/FuzzyEnum.java
+++ src/main/java/com/white/robot/warehouse/FuzzyEnum.java
@@ -8,8 +8,9 @@ import lombok.RequiredArgsConstructor;
 
 public enum FuzzyEnum {
     M1("男女比", "果园男女比2:1，当然脱单和男女比无关，具体请参见新生手册"),
-    M19("学妹", "学妹小心，不要听怪叔叔的话"),
+    M19("学妹", "学妹小心，他们都是怪蜀黍怪阿姨"),
     M21("学姐","请加可爱的白白学姐"),
+    M4("哐次","嘟嘟嘟，哐次哐次o(￣▽￣)ｄ，叫我干嘛"),
     M20("学弟", "学弟这个物种，懂得都懂");
 
     private final String req;
--- src/main/java/com/white/robot/warehouse/MessageEnum.java
+++ src/main/java/com/white/robot/warehouse/MessageEnum.java
@@ -10,9 +10,7 @@ public enum MessageEnum {
     M1("太难了", "会则不难，难则不会！\n" + "还是因为你不会嘛！"),
     M2("官方群", "学校是没有官方群的，说自己是官方群的都是骗子"),
     M3("可以","可以可以，这很哐次"),
-    M4("哐次","嘟嘟嘟，哐次哐次o(￣▽￣)ｄ，叫我干嘛"),
-    M5("哐次哐次","嘟嘟嘟，哐次哐次o(￣▽￣)ｄ，叫我干嘛"),
-    M100("新生手册", "https://docs.qq.com/doc/BLIGHi0op6F60KZQIK1hrUCb4nD98o3xmHNn0spAgc3ojLjX3vwexQ0eCYwB0qAvGk251TRI4");
+    M100("新生手册", "请自行查看群公告");
     private final String req;
     private final String resp;
 
